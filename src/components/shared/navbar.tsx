e685529af3f325e393775b49a2270014892a4c2c
Sitanshu Pokalwar <sitz2309@gmal.com>
2024-04-17 17:15:19 +0530

    add: basic ui

---
+++ src/components/shared/navbar.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import Link from "next/link";
+import { Button } from "../ui/button";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-background/60 sticky top-0 z-10 flex h-16 items-center gap-10 border-b px-4 backdrop-blur-xl transition-all">
+      <Link href="/" className="flex items-center space-x-2">
+        <span className="font-urban inline-block text-xl font-bold">
+          T3 Gallery
+        </span>
+      </Link>
+      <div className="ml-auto flex items-center space-x-4">
+        <Link href="/signin">
+          <Button className="relative rounded-lg">Sign In</Button>
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
