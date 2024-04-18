c5659a281f2887e2afff4abbb92349a58a6bad70
Sitanshu Pokalwar <sitz2309@gmal.com>
2024-04-18 18:27:38 +0530

    add: auth check for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,35 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
-  const images = await db.query.images.findMany({
-    orderBy: (model, { desc }) => desc(model.id),
-  });
+export default function HomePage() {
   return (
     <main className="p-4">
-      <div className="flex flex-wrap gap-4 ">
-        {[...images, ...images].map((image, index) => (
-          <div key={image.id + "-" + index} className="flex w-48  flex-col ">
-            <img src={image.url} alt="image" />
-            <p>{image.name}</p>
-          </div>
-        ))}
+      <div>
+        <SignedOut>
+          <p>please sign in</p>
+        </SignedOut>
+        <SignedIn>
+          <Images />
+        </SignedIn>
       </div>
     </main>
   );
 }
+
+const Images = async () => {
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
+  return (
+    <div className="flex flex-wrap gap-4 ">
+      {[...images, ...images].map((image, index) => (
+        <div key={image.id + "-" + index} className="flex w-48  flex-col ">
+          <img src={image.url} alt="image" />
+          <p>{image.name}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
