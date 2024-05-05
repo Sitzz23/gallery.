2cdd5169c0f45f74fd7d9dce40f0542f4f2300d2
Sitanshu Pokalwar <sitz2309@gmal.com>
2024-05-05 23:33:16 +0530

    wip: custom upload button

---
+++ src/app/_components/customUploadButton.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { Button } from "~/components/ui/button";
+import { useUploadThing } from "~/utils/uploadthing";
+
+// inferred input off useUploadThing
+type Input = Parameters<typeof useUploadThing>;
+
+const useUploadThingInputProps = (...args: Input) => {
+  const $ut = useUploadThing(...args);
+
+  const onChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) return;
+
+    const selectedFiles = Array.from(e.target.files);
+    const result = await $ut.startUpload(selectedFiles);
+
+    console.log("uploaded files", result);
+    // TODO: persist result in state maybe?
+  };
+
+  return {
+    inputProps: {
+      onChange,
+      multiple: ($ut.permittedFileInfo?.config?.image?.maxFileCount ?? 1) > 1,
+      accept: "image/*",
+    },
+    isUploading: $ut.isUploading,
+  };
+};
+
+export const CustomUploadButton = () => {
+  return (
+    <div>
+      <Button>Upload</Button>
+    </div>
+  );
+};
