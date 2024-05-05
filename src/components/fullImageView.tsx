4cd70bea7ff2f0e1a218e58da5cfe98f51149b53
Sitanshu Pokalwar <sitz2309@gmal.com>
2024-05-05 21:58:48 +0530

    add: function to fetch other images

--- src/components/fullImageView.tsx
+++ src/components/fullImageView.tsx
@@ -1,8 +1,10 @@
 /* eslint-disable @next/next/no-img-element */
 import { clerkClient } from "@clerk/nextjs/server";
 import { Button } from "~/components/ui/button";
-import { deleteImage, getImage } from "~/server/queries";
+import { deleteImage, getImage, getOtherImages } from "~/server/queries";
 import CopyToClipboardButton from "~/app/_components/copyToClipboard";
+import Link from "next/link";
+import Image from "next/image";
 
 const FullImageView = async ({ photoId }: { photoId: number }) => {
   const image = await getImage(Number(photoId));
@@ -10,9 +12,100 @@ const FullImageView = async ({ photoId }: { photoId: number }) => {
 
   return (
     <div className=" flex max-w-full justify-between gap-4 p-4">
-      <div className="flex grow flex-col items-end gap-4">
-        <div className="flex w-full items-center justify-between gap-2  text-sm">
-          <div className="flex items-center gap-2 rounded-md bg-neutral-200 p-3">
+      <div className="flex grow flex-col justify-between">
+        <div className="flex flex-col items-end justify-between gap-4">
+          <div className="flex w-full items-center justify-between gap-2 text-sm">
+            <div className="flex items-center gap-2 rounded-md bg-neutral-200 p-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="h-4 w-4"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M1 5.25A2.25 2.25 0 0 1 3.25 3h13.5A2.25 2.25 0 0 1 19 5.25v9.5A2.25 2.25 0 0 1 16.75 17H3.25A2.25 2.25 0 0 1 1 14.75v-9.5Zm1.5 5.81v3.69c0 .414.336.75.75.75h13.5a.75.75 0 0 0 .75-.75v-2.69l-2.22-2.219a.75.75 0 0 0-1.06 0l-1.91 1.909.47.47a.75.75 0 1 1-1.06 1.06L6.53 8.091a.75.75 0 0 0-1.06 0l-2.97 2.97ZM12 7a1 1 0 1 1-2 0 1 1 0 0 1 2 0Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              <h3 className="font-medium leading-none">{image.name}</h3>
+            </div>
+            <div className="flex items-center gap-2 rounded-md bg-neutral-200 p-3">
+              <span className="font-medium">✦</span>
+              <h3 className="font-medium leading-none">{userInfo.fullName}</h3>
+            </div>
+          </div>
+          <div className="flex items-center justify-end gap-4">
+            {image && (
+              <CopyToClipboardButton
+                stringToCopy={image.url}
+                itemCopied={image.name}
+              />
+            )}
+
+            <form
+              action={async () => {
+                "use server";
+                await deleteImage(photoId);
+              }}
+            >
+              <Button
+                type="submit"
+                variant="destructive"
+                className="flex items-center gap-1 rounded-md"
+              >
+                <svg
+                  width="17"
+                  height="17"
+                  viewBox="0 0 15 15"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M5.5 1C5.22386 1 5 1.22386 5 1.5C5 1.77614 5.22386 2 5.5 2H9.5C9.77614 2 10 1.77614 10 1.5C10 1.22386 9.77614 1 9.5 1H5.5ZM3 3.5C3 3.22386 3.22386 3 3.5 3H5H10H11.5C11.7761 3 12 3.22386 12 3.5C12 3.77614 11.7761 4 11.5 4H11V12C11 12.5523 10.5523 13 10 13H5C4.44772 13 4 12.5523 4 12V4L3.5 4C3.22386 4 3 3.77614 3 3.5ZM5 4H10V12H5V4Z"
+                    fill="currentColor"
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                <span>Delete</span>
+              </Button>
+            </form>
+          </div>
+        </div>
+        <div className="flex flex-col gap-2">
+          <span className="text-2xl font-black tracking-tighter">
+            More Images.
+          </span>
+          <OtherImages imageId={image.id} />
+        </div>
+      </div>
+      <img
+        src={image.url}
+        alt={image.name}
+        className={"h-[88.5vh] w-auto rounded-md object-contain object-top"}
+      />
+    </div>
+  );
+};
+
+const OtherImages = async ({ imageId }: { imageId: number }) => {
+  const images = await getOtherImages(imageId);
+
+  return (
+    <div className="flex flex-wrap justify-start gap-4 overflow-y-scroll">
+      {images.map((image) => (
+        <div key={image.id} className="flex flex-col gap-2 overflow-hidden">
+          <Link href={`/img/${image.id}`}>
+            <Image
+              src={image.url}
+              alt={image.name}
+              width={192}
+              height={364}
+              className={"aspect-square rounded-md object-cover object-top"}
+            />
+          </Link>
+          <div className="flex gap-2 rounded-md bg-neutral-100 p-3 text-sm">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 20 20"
@@ -25,56 +118,11 @@ const FullImageView = async ({ photoId }: { photoId: number }) => {
                 clipRule="evenodd"
               />
             </svg>
+
             <h3 className="font-medium leading-none">{image.name}</h3>
           </div>
-          <div className="flex items-center gap-2 rounded-md bg-neutral-200 p-3">
-            <span className="font-medium">✦</span>
-            <h3 className="font-medium leading-none">{userInfo.fullName}</h3>
-          </div>
         </div>
-        <div className="flex items-center justify-end gap-4">
-          {image && (
-            <CopyToClipboardButton
-              stringToCopy={image.url}
-              itemCopied={image.name}
-            />
-          )}
-
-          <form
-            action={async () => {
-              "use server";
-              await deleteImage(photoId);
-            }}
-          >
-            <Button
-              type="submit"
-              variant="destructive"
-              className="flex items-center gap-1 rounded-md"
-            >
-              <svg
-                width="17"
-                height="17"
-                viewBox="0 0 15 15"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M5.5 1C5.22386 1 5 1.22386 5 1.5C5 1.77614 5.22386 2 5.5 2H9.5C9.77614 2 10 1.77614 10 1.5C10 1.22386 9.77614 1 9.5 1H5.5ZM3 3.5C3 3.22386 3.22386 3 3.5 3H5H10H11.5C11.7761 3 12 3.22386 12 3.5C12 3.77614 11.7761 4 11.5 4H11V12C11 12.5523 10.5523 13 10 13H5C4.44772 13 4 12.5523 4 12V4L3.5 4C3.22386 4 3 3.77614 3 3.5ZM5 4H10V12H5V4Z"
-                  fill="currentColor"
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-              <span>Delete</span>
-            </Button>
-          </form>
-        </div>
-      </div>
-      <img
-        src={image.url}
-        alt={image.name}
-        className={"h-[88.5vh] w-auto rounded-md object-contain object-top"}
-      />
+      ))}
     </div>
   );
 };
