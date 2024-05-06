aa186ee0a6abf7eeed253ead4def088ec58dd135
Sitanshu Pokalwar <sitz2309@gmal.com>
2024-05-06 20:28:18 +0530

    fix: code cleaning

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -8,14 +8,21 @@ const Navbar = () => {
   return (
     <nav className="bg-background/60 sticky top-0 z-10 flex h-16 items-center gap-10 px-4 backdrop-blur-xl transition-all">
       <Link href="/" className="flex items-center space-x-2">
-        <span className="inline-block rounded-md py-0.5 pr-2 text-left text-3xl font-black tracking-tighter underline">
-          gallery.
+        <span className="flex items-center justify-start gap-1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="mt-1 h-5 w-5"
+          >
+            <path d="M11.983 1.907a.75.75 0 0 0-1.292-.657l-8.5 9.5A.75.75 0 0 0 2.75 12h6.572l-1.305 6.093a.75.75 0 0 0 1.292.657l8.5-9.5A.75.75 0 0 0 17.25 8h-6.572l1.305-6.093Z" />
+          </svg>
+          <span className="inline-block rounded-md py-0.5 text-left text-2xl font-black tracking-tighter  ">
+            gallery.
+          </span>
         </span>
       </Link>
       <div className="ml-auto flex items-center space-x-4">
-        {/* <Link href="/signin">
-          <Button className="relative rounded-lg">Sign In</Button>
-        </Link> */}
         <SignedOut>
           <Button variant={"default"}>
             <SignInButton />
@@ -24,12 +31,6 @@ const Navbar = () => {
         <SignedIn>
           <div className="flex items-center justify-center gap-4">
             <CustomUploadButton />
-            {/* <UploadButton
-              endpoint="imageUploader"
-              onClientUploadComplete={() => {
-                router.refresh();
-              }}
-            /> */}
           </div>
           <UserButton />
         </SignedIn>
